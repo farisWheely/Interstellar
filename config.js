fe8ee02cf5cd1d6bc9ace793785f92fb60fb3d3d
farisWheely <50304957+farisWheely@users.noreply.github.com>
2025-02-02 23:53:34 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,27 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    guest: "4321",
+    USER: "5678",
+    TEST: "9101",
+    DEMO: "1121",
+    DATA: "3141",
+    INFO: "5161",
+    CODE: "7181",
+    PASS: "9202",
+    LOCK: "2232",
+    OPEN: "4252",
+    LIST: "6272",
+    ITEM: "8292",
+    FILE: "0313",
+    TEMP: "2333",
+    NAME: "4353",
+    SAVE: "6373",
+    VIEW: "8393",
+    TEXT: "0414",
+    EXIT: "2434",
+    DONE: "4454", // Removed trailing comma here
   },
 };
 
